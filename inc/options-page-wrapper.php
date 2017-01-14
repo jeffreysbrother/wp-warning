572f5b37651e09a4240b218c61fea7f01fb1f7e0
James Cool <jeffreysbrother@gmail.com>
2017-01-13 16:41:44 -0800

    eliminate default content

--- inc/options-page-wrapper.php
+++ inc/options-page-wrapper.php
@@ -66,10 +66,7 @@
 								); ?></span></h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e(
-									'Everything you see here, from the documentation to the code itself, was created by and for the community. WordPress is an Open Source project, which means there are hundreds of people all over the world working on it. (More than most commercial platforms.) It also means you are free to use it for anything from your cat’s home page to a Fortune 500 web site without paying anyone a license fee and a number of other important freedoms.',
-									'wp_admin_style'
-								); ?></p>
+							<p>Stuff goes here too...</p>
 						</div>
 						<!-- .inside -->
 
